4be607153ea4f8c82836161f05b0e72ea3547ff3
Nathan Henrique Lima Chaves <111211074+NathanHenriqueLimaChaves@users.noreply.github.com>
2022-10-19 00:19:39 -0300

    Add files via upload

---
+++ JavaScript/javascript_codigo_basico.js
@@ -0,0 +1,14 @@
+// coisas básicas
+
+let nome = "Nathan";
+let idade = 24;
+const anoNascimento = 1998;
+
+console.log(nome, idade, anoNascimento);
+
+const PI = 3.1415;
+console.log(PI);
+
+console.log(23 + 34 + 21 - 10);
+
+
