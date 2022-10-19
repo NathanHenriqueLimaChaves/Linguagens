4275ac59d3244b0e83bcc7b8ba8a9eb075569d02
Nathan Henrique Lima Chaves <111211074+NathanHenriqueLimaChaves@users.noreply.github.com>
2022-10-19 00:11:46 -0300

    Add files via upload

---
+++ JavaScript/10-2022/script02.js
@@ -0,0 +1,35 @@
+// Continuando meu estudo da linguagem JavaScript - 18/10/2022
+
+true;
+console.log(true);
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 28);
+console.log(typeof "Nathan");
+
+javaScriptIsFun = "Yes!";
+console.log(javaScriptIsFun);
+
+let myPetName;
+console.log(typeof myPetName);
+
+myPetName = "Luna";
+console.log(myPetName);
+
+
+
+//let, const ans var ********************
+
+let age = 30;
+age = 31;
+
+const birthYear = 1998;
+// birthYear = 1991;  -> irá dar erro
+//const job; -> também dará erro
+
+
+var job = "Programmer"; // é a maneira antiga, evitar usar
+job = "Teacher"
