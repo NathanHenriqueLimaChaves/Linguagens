365ae9fac41c6b9596b3cb4d8e99e238c0ee2c62
Nathan Henrique Lima Chaves <111211074+NathanHenriqueLimaChaves@users.noreply.github.com>
2022-10-26 21:47:20 -0400

    Add files via upload

---
+++ JavaScript/10-2022/resumo_basico_js(1).js
@@ -0,0 +1,10 @@
+// Resumo básico
+
+var nome = 'Nathan'; // var é antigo, dar preferência para o let e const
+let sobrenome = 'Henrique';
+let idade = 24;
+const ano_nascimento = 1998;
+const id_func = 0124;
+
+console.log("O funcionário de ID:" + id_func + ", chama-se " + nome + " " + sobrenome + ". Nascido no ano de " + ano_nascimento + ", Nathan tem " + idade + " anos.")
+console.log("No ano de 2078, " + nome + " terá " + (2078 - ano_nascimento) + " anos.")
