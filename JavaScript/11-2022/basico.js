ae7982b7db66c9be678ddddc5f5dbc90b257ba17
Nathan Henrique Lima Chaves <111211074+NathanHenriqueLimaChaves@users.noreply.github.com>
2022-11-06 22:20:39 -0400

    Add files via upload

---
+++ JavaScript/11-2022/basico.js
@@ -0,0 +1,20 @@
+
+
+let sorvete = 'chocolate';
+if (sorvete === 'chocolate') {
+  console.log('Opa, Eu amo sorvete de chocolate!');
+} else {
+  console.log('Ahh, mas chocolate é o meu favorito...');
+}
+
+
+function multiplica(num1,num2) {
+  let resultado = num1 * num2;
+  return resultado;
+}
+
+
+multiplica(4, 7);
+multiplica(20, 20);
+multiplica(0.5, 3);
+
